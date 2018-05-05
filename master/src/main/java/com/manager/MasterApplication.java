ceb36db0141ee75c6613daf1402133341fc51531
wuyu616818 <wuyu616818@163.com>
2018-05-05 20:34:41 +0800

    事务管理添加

--- master/src/main/java/com/manager/MasterApplication.java
+++ master/src/main/java/com/manager/MasterApplication.java
@@ -3,7 +3,9 @@ package com.manager;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 @SpringBootApplication
 @MapperScan("com.manager.master.dao")
 public class MasterApplication {
