f3059007089a178ed6579dbcc00e3f4e2778184f
wuyu616818 <wuyu616818@163.com>
2018-05-17 18:22:04 +0800

    群组日程添加BUG REPAIR

--- TimeManager/app/src/main/assets/js/schedule/add_schedule.js
+++ TimeManager/app/src/main/assets/js/schedule/add_schedule.js
@@ -21,12 +21,35 @@ function addClick() {
 
     var createGroup = document.getElementById("createGroup");
     var groupIndex = createGroup.selectedIndex;
-    var flagFocus = createGroup.options[groupIndex].value;
+    var flagCreateGroup = createGroup.options[groupIndex].value;
     var focus = document.getElementById("focus");
     var focusIndex = focus.selectedIndex;
-    var flagCreateGroup =focus.options[focusIndex].value;
+    var flagFocus =focus.options[focusIndex].value;
 
-    window.index_schedule.add_Schedule(scheduleName, scheduleDetail, scheduleStartDate, scheduleEndDate, scheduleRemindDate, scheduleRemindRepeatType, executor, flagFocus, flagCreateGroup);
+    window.index_schedule.add_Schedule(scheduleName, scheduleDetail, scheduleStartDate, scheduleEndDate, scheduleRemindDate, scheduleRemindRepeatType, executor, flagCreateGroup, flagFocus);
+}
+
+function addGroupClick() {
+    var scheduleName = document.getElementById("schedule_name").value;
+    var scheduleDetail = document.getElementById("schedule_detail").value;
+    var scheduleStartDate = document.getElementById("pickdate").value + " " + document.getElementById("picktime").value;
+    var scheduleEndDate = document.getElementById("pickdate2").value + " " + document.getElementById("picktime2").value;
+
+    var remind = document.getElementById("remind_time");
+    var index = remind.selectedIndex;
+    var scheduleRemindDate = remindTimeSpilt(document.getElementById("pickdate").value, document.getElementById("picktime").value, remind.options[index].value);
+
+    var remind_repeat = document.getElementById("remind_repeat");
+    var index_repeat = remind_repeat.selectedIndex;
+    var scheduleRemindRepeatType = remind_repeat.options[index_repeat].value;
+
+    var executor = document.getElementById("schedule_person").value;
+
+    var focus = document.getElementById("focus");
+    var focusIndex = focus.selectedIndex;
+    var flagFocus =focus.options[focusIndex].value;
+
+    window.group_schedule.add_Schedule_Group(scheduleName, scheduleDetail, scheduleStartDate, scheduleEndDate, scheduleRemindDate, scheduleRemindRepeatType, executor, flagFocus);
 }
 
 function remindDate() {
